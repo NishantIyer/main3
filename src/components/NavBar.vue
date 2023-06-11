6597545e054e00422ccaeeb491404d7a50b89514
n1sh <88764215+NishantIyer@users.noreply.github.com>
2023-06-11 12:54:34 +0530

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,47 +1,54 @@
 <template>
-  <header class="header">
-    <nav class="nav">
-      <div class="left">
-        <router-link to="/" title="Home" class="nav-link">
-          <span class="nav-icon">
-            <i class="i-uil:home-alt"></i>
-          </span>
-        </router-link>
-      </div>
-      <div class="center">
-        <router-link to="/posts" title="Blog" class="nav-link">
-          <span class="nav-text">Blog</span>
-        </router-link>
-        <router-link to="/academia" title="Academia" class="nav-link">
-          <span class="nav-text">Academia</span>
-        </router-link>
-        <router-link to="/projects" title="Projects" class="nav-link">
-          <span class="nav-text">Projects</span>
-        </router-link>
-      </div>
-      <div class="right">
-        <a href="https://github.com/NishantIyer" target="_blank" title="GitHub" class="nav-link">
-          <span class="nav-icon">
-            <i class="i-uil-github-alt"></i>
-          </span>
-        </a>
-        <a href="https://nishantiyer.netlify.app/pics" target="_blank" title="Pictures" class="nav-link">
-          <span class="nav-icon">
-            <i class="i-uil:picture"></i>
-          </span>
-        </a>
-      </div>
-    </nav>
-  </header>
+  <div class="container">
+    <header class="header">
+      <nav class="nav">
+        <div class="left">
+          <router-link to="/" title="Home" class="nav-link">
+            <span class="nav-icon">
+              <i class="i-uil:home-alt"></i>
+            </span>
+          </router-link>
+        </div>
+        <div class="center">
+          <router-link to="/posts" title="Blog" class="nav-link">
+            <span class="nav-text">Blog</span>
+          </router-link>
+          <router-link to="/academia" title="Academia" class="nav-link">
+            <span class="nav-text">Academia</span>
+          </router-link>
+          <router-link to="/projects" title="Projects" class="nav-link">
+            <span class="nav-text">Projects</span>
+          </router-link>
+        </div>
+        <div class="right">
+          <a href="https://github.com/NishantIyer" target="_blank" title="GitHub" class="nav-link">
+            <span class="nav-icon">
+              <i class="i-uil-github-alt"></i>
+            </span>
+          </a>
+          <a href="https://nishantiyer.netlify.app/pics" target="_blank" title="Pictures" class="nav-link">
+            <span class="nav-icon">
+              <i class="i-uil:picture"></i>
+            </span>
+          </a>
+        </div>
+      </nav>
+    </header>
+  </div>
 </template>
 
 <style scoped>
+.container {
+  max-width: 100%;
+  overflow-x: hidden;
+}
+
 .header {
   position: relative;
 }
 
 .nav {
-  padding: 1.5rem;
+  padding: 1rem;
   width: 100%;
   display: flex;
   justify-content: space-between;
@@ -70,8 +77,8 @@
   text-decoration: none;
   color: #fff;
   font-weight: bold;
-  font-size: 16px;
-  padding: 10px 16px;
+  font-size: 14px;
+  padding: 8px 12px;
   border-radius: 8px;
   transition: background-color 0.2s ease;
 }
@@ -89,8 +96,8 @@
 }
 
 .nav-icon {
-  font-size: 18px;
-  margin-right: 8px;
+  font-size: 16px;
+  margin-right: 6px;
 }
 
 .nav-text {
@@ -100,6 +107,28 @@
 
 .nav-icon i {
   vertical-align: middle;
+}
+
+@media (max-width: 480px) {
+  .nav {
+    flex-wrap: wrap;
+    padding: 1rem 0;
+  }
+
+  .left,
+  .center,
+  .right {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 1rem;
+  }
+
+  .nav-link {
+    font-size: 16px;
+    padding: 8px 12px;
+    margin: 0 8px;
+  }
 }
 </style>
 
