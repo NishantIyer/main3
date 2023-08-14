5edca88cc8703e8b9c4b281ffec290fe061704b7
Nishant Iyer <88764215+NishantIyer@users.noreply.github.com>
2023-08-14 19:20:44 +0530

    Update Plum.vue

--- src/components/Plum.vue
+++ src/components/Plum.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="background">
-    <canvas ref="el" :width="size.width" :height="size.height" @mousemove="handleMouseMove" />
+    <canvas ref="el" :width="size.width" :height="size.height" @mousemove="handleMouseMove" @touchmove="handleTouchMove" />
   </div>
 </template>
 
@@ -18,36 +18,41 @@ const interactiveRadiusSquared = interactiveRadius * interactiveRadius
 interface Dot {
   x: number
   y: number
-  startX: number
-  startY: number
+  startX: number // Store initial x position
+  startY: number // Store initial y position
   radius: number
+  color: string
+  dx: number
+  dy: number
   originalRadius: number
   angle: number
-  speed: number
 }
 
 function createDot(): Dot {
   const x = Math.random() * size.width
   const y = Math.random() * size.height
-  const radius = Math.random() * 4 + 2
+  const radius = Math.random() * 3 + 1
   const originalRadius = radius
+  const color = 'rgba(220, 220, 220, 0.8)' // More dim white with transparency
+  const dx = (Math.random() - 0.5) * 2
+  const dy = (Math.random() - 0.5) * 2
   const angle = Math.random() * Math.PI * 2
-  const speed = Math.random() * 1 + 0.5
 
-  return { x, y, startX: x, startY: y, radius, originalRadius, angle, speed }
+  return { x, y, startX: x, startY: y, radius, color, dx, dy, originalRadius, angle }
 }
 
 function updateDots(): void {
   dots.forEach((dot) => {
-    dot.x += Math.cos(dot.angle) * dot.speed
-    dot.y += Math.sin(dot.angle) * dot.speed
+    dot.x += dot.dx
+    dot.y += dot.dy
+    dot.angle += 0.01 // Increase the angle for rotation
 
     if (dot.x + dot.radius > size.width || dot.x - dot.radius < 0) {
-      dot.angle = Math.PI - dot.angle
+      dot.dx = -dot.dx
     }
 
     if (dot.y + dot.radius > size.height || dot.y - dot.radius < 0) {
-      dot.angle = -dot.angle
+      dot.dy = -dot.dy
     }
   })
 }
@@ -57,11 +62,12 @@ function drawDots(ctx: CanvasRenderingContext2D): void {
   dots.forEach((dot) => {
     ctx.save()
     ctx.translate(dot.x, dot.y)
+    ctx.rotate(dot.angle) // Rotate the dot
     ctx.beginPath()
     ctx.arc(0, 0, dot.radius, 0, Math.PI * 2)
-    ctx.fillStyle = '#444' // Darker gray color
-    ctx.shadowColor = '#000' // Black shadow
-    ctx.shadowBlur = 20
+    ctx.fillStyle = dot.color
+    ctx.shadowColor = 'rgba(0, 0, 0, 0.5)' // Darker shadow color with transparency
+    ctx.shadowBlur = dot.radius * 2 // Adjusted shadow blur
     ctx.fill()
     ctx.restore()
   })
@@ -75,13 +81,29 @@ function handleMouseMove(event: MouseEvent): void {
     const distanceSquared = (mouseX - dot.x) ** 2 + (mouseY - dot.y) ** 2
     if (distanceSquared < interactiveRadiusSquared) {
       const angle = Math.atan2(dot.y - mouseY, dot.x - mouseX)
-      dot.x += Math.cos(angle) * 2 // Move away from mouse
-      dot.y += Math.sin(angle) * 2 // Move away from mouse
-      dot.radius = dot.originalRadius - 2 // Shrink the dot
-    } else if (dot.radius < dot.originalRadius) {
-      dot.radius += 0.5 // Gradually return to original size
-      dot.x += (dot.startX - dot.x) * 0.02 // Return to initial x position
-      dot.y += (dot.startY - dot.y) * 0.02 // Return to initial y position
+      dot.dx = Math.cos(angle) * 0.3 // Slowly move away from mouse
+      dot.dy = Math.sin(angle) * 0.3 // Slowly move away from mouse
+    } else {
+      dot.dx = (dot.startX - dot.x) * 0.02 // Move back to initial x position
+      dot.dy = (dot.startY - dot.y) * 0.02 // Move back to initial y position
+    }
+  })
+}
+
+function handleTouchMove(event: TouchEvent): void {
+  const touch = event.touches[0]
+  const touchX = touch.pageX
+  const touchY = touch.pageY
+
+  dots.forEach((dot) => {
+    const distanceSquared = (touchX - dot.x) ** 2 + (touchY - dot.y) ** 2
+    if (distanceSquared < interactiveRadiusSquared) {
+      const angle = Math.atan2(dot.y - touchY, dot.x - touchX)
+      dot.dx = Math.cos(angle) * 0.3 // Slowly move away from touch
+      dot.dy = Math.sin(angle) * 0.3 // Slowly move away from touch
+    } else {
+      dot.dx = (dot.startX - dot.x) * 0.02 // Move back to initial x position
+      dot.dy = (dot.startY - dot.y) * 0.02 // Move back to initial y position
     }
   })
 }
@@ -92,7 +114,7 @@ onMounted(() => {
   canvas.width = size.width
   canvas.height = size.height
 
-  for (let i = 0; i < 30; i++) {
+  for (let i = 0; i < 20; i++) {
     dots.push(createDot())
   }
 
@@ -115,7 +137,7 @@ onMounted(() => {
   bottom: 0;
   z-index: -1;
   pointer-events: none;
-  background-color: #000;
+  background-color: #000000;
 }
 
 canvas {
