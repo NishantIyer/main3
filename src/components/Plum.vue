2a0740595e8237939672a70928a8e8c07918c57a
Nishant Iyer <88764215+NishantIyer@users.noreply.github.com>
2023-08-14 18:11:42 +0530

    Update Plum.vue

--- src/components/Plum.vue
+++ src/components/Plum.vue
@@ -12,31 +12,35 @@ const el = ref<HTMLCanvasElement | null>(null)
 const size = reactive(useWindowSize())
 
 let dots: Dot[] = []
-const interactiveRadius = 80
+const interactiveRadius = 100
 const interactiveRadiusSquared = interactiveRadius * interactiveRadius
 
 interface Dot {
   x: number
   y: number
+  startX: number
+  startY: number
   radius: number
   originalRadius: number
   angle: number
+  speed: number
 }
 
 function createDot(): Dot {
   const x = Math.random() * size.width
   const y = Math.random() * size.height
-  const radius = Math.random() * 3 + 1
+  const radius = Math.random() * 4 + 2
   const originalRadius = radius
   const angle = Math.random() * Math.PI * 2
+  const speed = Math.random() * 1 + 0.5
 
-  return { x, y, radius, originalRadius, angle }
+  return { x, y, startX: x, startY: y, radius, originalRadius, angle, speed }
 }
 
 function updateDots(): void {
   dots.forEach((dot) => {
-    dot.x += Math.cos(dot.angle) * 0.5
-    dot.y += Math.sin(dot.angle) * 0.5
+    dot.x += Math.cos(dot.angle) * dot.speed
+    dot.y += Math.sin(dot.angle) * dot.speed
 
     if (dot.x + dot.radius > size.width || dot.x - dot.radius < 0) {
       dot.angle = Math.PI - dot.angle
@@ -55,9 +59,9 @@ function drawDots(ctx: CanvasRenderingContext2D): void {
     ctx.translate(dot.x, dot.y)
     ctx.beginPath()
     ctx.arc(0, 0, dot.radius, 0, Math.PI * 2)
-    ctx.fillStyle = '#333' // Dark gray color
+    ctx.fillStyle = '#444' // Darker gray color
     ctx.shadowColor = '#000' // Black shadow
-    ctx.shadowBlur = 15
+    ctx.shadowBlur = 20
     ctx.fill()
     ctx.restore()
   })
@@ -70,10 +74,14 @@ function handleMouseMove(event: MouseEvent): void {
   dots.forEach((dot) => {
     const distanceSquared = (mouseX - dot.x) ** 2 + (mouseY - dot.y) ** 2
     if (distanceSquared < interactiveRadiusSquared) {
-      dot.radius = dot.originalRadius + 3 // Enlarge the dot
-      dot.angle += 0.1 // Spin the dot
-    } else if (dot.radius > dot.originalRadius) {
-      dot.radius -= 0.5 // Gradually return to original size
+      const angle = Math.atan2(dot.y - mouseY, dot.x - mouseX)
+      dot.x += Math.cos(angle) * 2 // Move away from mouse
+      dot.y += Math.sin(angle) * 2 // Move away from mouse
+      dot.radius = dot.originalRadius - 2 // Shrink the dot
+    } else if (dot.radius < dot.originalRadius) {
+      dot.radius += 0.5 // Gradually return to original size
+      dot.x += (dot.startX - dot.x) * 0.02 // Return to initial x position
+      dot.y += (dot.startY - dot.y) * 0.02 // Return to initial y position
     }
   })
 }
@@ -84,7 +92,7 @@ onMounted(() => {
   canvas.width = size.width
   canvas.height = size.height
 
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 30; i++) {
     dots.push(createDot())
   }
 
