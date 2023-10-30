8f60249070372b1b95343bac042729483d8a94e2
Nishant Iyer <88764215+NishantIyer@users.noreply.github.com>
2023-10-30 21:49:21 +0530

    Create auth.vue

---
+++ src/components/auth.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <div v-if="loggedIn" class="content">
+      Hey, Kindly visit <a :href="link" target="_blank">{{ link }}</a>
+    </div>
+    <div v-else class="login-container glass-effect">
+      <div class="hint">Hint: L.br..t..n</div>
+      <input type="password" v-model="password" placeholder="Enter Password" />
+      <button @click="login">Log In</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      password: "",
+      loggedIn: false,
+      link: "https://ny.netlify.app",
+    };
+  },
+  methods: {
+    login() {
+      if (this.password === "Lubrication") {
+        this.loggedIn = true;
+      } else {
+        alert("Incorrect password");
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  background-color: #333;
+  color: white;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.glass-effect {
+  backdrop-filter: blur(20px) saturate(200%);
+  background-color: rgba(0, 0, 0, 0.3);
+  border-radius: 15px;
+  padding: 20px;
+  width: 300px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.hint {
+  margin-bottom: 10px;
+  font-size: 1.2em;
+}
+
+input {
+  padding: 10px;
+  margin-bottom: 10px;
+  border: none;
+  border-radius: 5px;
+}
+
+button {
+  padding: 10px;
+  background-color: #555;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.content {
+  font-size: 1.2em;
+}
+</style>
