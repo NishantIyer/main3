77b6b82daf41406c65331b8bc6bc21ada50c49a1
Nishant Iyer <88764215+NishantIyer@users.noreply.github.com>
2025-07-13 21:07:00 +0530

    Update PicsPage.vue

--- src/components/PicsPage.vue
+++ src/components/PicsPage.vue
@@ -41,133 +41,16 @@
 export default {
   data() {
     return {
-      years: ["2023", "2022", "2021", "Projects", "Other"],
-      selectedYear: "2023",
+      years: ["Projects"],
+      selectedYear: "Projects",
       images: [
-        {
-          year: "2023",
-          photos: [
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1140642686253150248/Screenshot_from_2023-07-08_08-20-58.png?width=440&height=424",
-              description: "dw..i didn't draw it",
-              showDetails: false,
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1179843417048612865/Screenshot_from_2023-10-20_08-34-37.png?ex=657b41cf&is=6568cccf&hm=1cb3cad353936d6d03b95d3d2cd8e49c605fbd008fdf4b74f30d827c5cc6bab2&=&format=webp&quality=lossless&width=442&height=373"
-            },
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116236755486650428/Screenshot_from_2023-05-31_10-47-18.png?width=366&height=490",
-              description: "May 2023",
-              showDetails: false,
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1116236755486650428/Screenshot_from_2023-05-31_10-47-18.png?width=366&height=490"
-            },
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1144565534814380072/image.png?width=324&height=376",
-              description: "July 2023",
-              showDetails: false,
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1144565534814380072/image.png?width=324&height=376"
-            },
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1144563765854085170/download.png?width=232&height=271",
-              description: "Aug 2023",
-              showDetails: false,
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1144563765854085170/download.png?width=232&height=271"
-            },
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1115957382254710824/cheek.png?width=344&height=491",
-              description: "1 month of mewing hard",
-              showDetails: false,
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1115957382254710824/cheek.png?width=344&height=491"
-            },
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1147471774980788305/Screenshot_from_2023-09-02_15-01-45.png?width=361&height=424",
-              description: "Aug 2023",
-              showDetails: false,
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1147471774980788305/Screenshot_from_2023-09-02_15-01-45.png?width=361&height=424"
-            },
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1147471774615875654/Screenshot_from_2023-09-02_14-59-09.png?width=361&height=424",
-              description: "Aug 2023",
-              showDetails: false,
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1147471774615875654/Screenshot_from_2023-09-02_14-59-09.png?width=361&height=424"
-            },
-            {
-              url: "https://cdn.discordapp.com/attachments/971299427715272734/1162677211816542218/image.png?ex=653cce88&is=652a5988&hm=c71908f2003bac2e6c63d2ad564280badbd4fd04c430df545e3f594a22bf8cff&",
-              description: "Oct 2023",
-              showDetails: false,
-              likes: 0,
-              link: "https://cdn.discordapp.com/attachments/971299427715272734/1162677211816542218/image.png?ex=653cce88&is=652a5988&hm=c71908f2003bac2e6c63d2ad564280badbd4fd04c430df545e3f594a22bf8cff&"
-            }
-          ]
-        },
-        {
-          year: "2022",
-          photos: [
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116237331712725072/nishhh.png?width=368&height=491",
-              description: "",
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1116237331712725072/nishhh.png?width=368&height=491"
-            },
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116298856502669332/Screenshot_from_2023-05-19_20-05-40.png?width=431&height=491",
-              description: "",
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1116298856502669332/Screenshot_from_2023-05-19_20-05-40.png?width=431&height=491"
-            },
-            {
-              url: "https://cdn.discordapp.com/attachments/971299427715272734/1126546017081368686/Screenshot_from_2023-06-05_15-13-44.png",
-              description: "oops..bad pic",
-              likes: 0,
-              link: "https://cdn.discordapp.com/attachments/971299427715272734/1126546017081368686/Screenshot_from_2023-06-05_15-13-44.png"
-            },
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1181290163943518298/Screenshot_20230301_092012.png?ex=65808532&is=656e1032&hm=cd5d36281f5dae0d5405dbe28bf2a3bc4079625c2864b129bbfabc43f0d5025d&=&format=webp&quality=lossless&width=306&height=345",
-              description: "oops..bad pic",
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1181290163943518298/Screenshot_20230301_092012.png?ex=65808532&is=656e1032&hm=cd5d36281f5dae0d5405dbe28bf2a3bc4079625c2864b129bbfabc43f0d5025d&=&format=webp&quality=lossless&width=306&height=345"
-            }
-          
-          ]
-        },
-        {
-          year: "2021",
-          photos: [
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116299894051844136/Screenshot_from_2023-06-08_15-06-58.png?width=229&height=277",
-              description: "cuteness overload",
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1116299894051844136/Screenshot_from_2023-06-08_15-06-58.png?width=229&height=277"
-            },
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116299893544333322/Screenshot_from_2023-06-08_15-06-22.png?width=331&height=390",
-              description: "",
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1116299893544333322/Screenshot_from_2023-06-08_15-06-22.png?width=331&height=390"
-            },
         
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116300410261622804/Screenshot_from_2023-06-08_15-08-45.png?width=229&height=277",
-              description: "",
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1116300410261622804/Screenshot_from_2023-06-08_15-08-45.png?width=229&height=277"
-            },
-            
-            
-          
-          ]
-        },
         {
           year: "Projects",
           photos: [
             {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116383468679614565/Screenshot_from_2023-06-08_20-38-41.png?width=268&height=246",
-              description: "",
+              url: "https://media.discordapp.net/attachments/971299427715272734/1393974907175567501/Screenshot_2025-07-13_203935.png?ex=68751fcc&is=6873ce4c&hm=5fe5f242bd6830c1bfdb959d9159b955488d510cc2281182b2e1751665fd3af5&=&format=webp&quality=lossless&width=762&height=678",
+              description: "Automatic luminance based solar tracker for equatorial solar energy. 2019 Summer of Arduino Project.",
               showDetails: false,
               likes: 0,
               link: "https://drive.google.com/file/d/1ptoib5G9sd63z1UmxJmcXGFMj4IkVrQQ/view?usp=drive_link"
@@ -176,82 +59,40 @@ export default {
               url: "https://media.discordapp.net/attachments/971299427715272734/1116384004371927201/Screenshot_from_2023-06-08_20-40-11.png?width=430&height=341",
               description: "",
               showDetails: false,
-              likes: 0,
-              link: "https://drive.google.com/file/d/1cto9VRn1o-wbGKDdhm87zpbx0hTW_ZHf/view?usp=drive_link"
+              likes: 30,
+              link: "https://drive.google.com/file/d/1ptoib5G9sd63z1UmxJmcXGFMj4IkVrQQ/view?usp=sharing"
             },
             
             {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116384043613814794/e06b1a76-11fc-4af5-9aea-927de07f825e.jpg?width=722&height=406",
-              description: "",
+              url: "https://media.discordapp.net/attachments/971299427715272734/1393974907443871825/Screenshot_2025-07-13_204032.png?ex=68751fcc&is=6873ce4c&hm=0a7845fa2f91f78cbca09b0ebf587e162f2d66cca170dc8726fcd114d2cc0358&=&format=webp&quality=lossless&width=845&height=689",
+              description: "SOAR Config, 2019 Noktoberfest Hackathon",
               showDetails: false,
               likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1116384043613814794/e06b1a76-11fc-4af5-9aea-927de07f825e.jpg?width=722&height=406"
+              link: "https://drive.google.com/file/d/11NLwSjfZLaE2gRKfj_wzKSX_5V37oeOu/view?usp=sharing"
             },
             {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116384044209422366/bios.jpeg?width=455&height=270",
-              description: "",
+              url: "https://media.discordapp.net/attachments/971299427715272734/1393974907712569526/Screenshot_2025-07-13_204055.png?ex=68751fcc&is=6873ce4c&hm=b0dd51b6acfdf7a112ab1105a4448c02be2dfbc55c96210d6cca6724e3432276&=&format=webp&quality=lossless&width=987&height=590",
+              description: "My attempt to simulate the working of an avionics compt. The JFHET part failed misrable but the IRNK part worked well.",
               showDetails: false,
               likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1116384044209422366/bios.jpeg?width=455&height=270"
+              link: "https://drive.google.com/file/d/1p6Us2HnEsUKvkRXTM8k_7UWY56LR43sL/view?usp=sharing"
             },
             {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116384044486250536/2018.04.03.png?width=449&height=354",
-              description: "",
+              url: "https://media.discordapp.net/attachments/971299427715272734/1393974908500836482/Screenshot_2025-07-13_204736.png?ex=68751fcc&is=6873ce4c&hm=c22cbe831417b1ab8b014fcd002fb86582a4d0ccaa1bda811cc64a93ebddc529&=&format=webp&quality=lossless&width=1227&height=689",
+              description: "A nueral network built by me before chatgpt was made pulbic using a variety of APIs.",
               showDetails: false,
               likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1116384044486250536/2018.04.03.png?width=449&height=354"
+              link: "https://drive.google.com/file/d/18Th6fXLP8RckZ1gkVicsZmGMzbakKMob/view?usp=sharing"
             },
             {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116384004594221078/Screenshot_from_2023-06-08_20-40-44.png?width=257&height=341",
-              description: "",
+              url: "https://media.discordapp.net/attachments/971299427715272734/1393978486795931699/1719080973957.jpg?ex=68752322&is=6873d1a2&hm=2fbce1815dbdebc570ca3fa2a5d4971bcea98b42ad689eacabdc6da240f2fba4&=&format=webp&width=1224&height=689",
+              description: "Me after Cascade EMR Prototype got selected in T hub one day before maths board exam",
               showDetails: false,
               likes: 0,
-              link: "https://drive.google.com/file/d/10wJo3b0oCFNnBsArccK17-sHESmb7xla/view?usp=drive_link"
-            },
+              link: "https://drive.google.com/file/d/1wYnnbtpc7E_nhoiSmxCAInAkBI6neLz7/view?usp=sharing"
+            },            
           ]
         },
-        {
-          year: "Other",
-          photos: [
-          
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116386346190258246/bd5e9c581dde6626b7d996b0f84f94bb.jpg?width=209&height=405",
-              description: "more than your hair",
-              likes: 0,
-              link: ""
-            },
-            {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1140642549510459392/Screenshot_from_2023-08-12_14-35-59.png?width=352&height=441",
-              description: "",
-              likes: 0,
-              link: ""
-            },
-          {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1116385993659011173/98e5b039a7b38f6f4ed0590db76dd29a.jpg?width=405&height=406",
-              description: "just sculpted",
-              likes: 0,
-              link: ""
-            },
-          {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1179848668585668729/download_8.png?ex=657b46b3&is=6568d1b3&hm=0e2eccaec0f0eeaa5bb49520ba06c77424f119e4575a38727c13a03e07a7f426&=&format=webp&quality=lossless&width=271&height=203",
-              description: "",
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1179848668585668729/download_8.png?ex=657b46b3&is=6568d1b3&hm=0e2eccaec0f0eeaa5bb49520ba06c77424f119e4575a38727c13a03e07a7f426&=&format=webp&quality=lossless&width=271&height=203"
-            },
-          {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1179849013349056562/download_5.png?ex=657b4705&is=6568d205&hm=ed4f6796eb55263f2c596ff3c44f6b30df2f0468a6d52f3c90a5944f0089aac2&=&format=webp&quality=lossless&width=221&height=299",
-              description: "",
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1179849013349056562/download_5.png?ex=657b4705&is=6568d205&hm=ed4f6796eb55263f2c596ff3c44f6b30df2f0468a6d52f3c90a5944f0089aac2&=&format=webp&quality=lossless&width=221&height=299"
-
-            },
-          {
-              url: "https://media.discordapp.net/attachments/971299427715272734/1179849642964426873/image.png?ex=657b479b&is=6568d29b&hm=679745322994208e25ce54508ed02ac353cbbeeb1c049b6fb5329b2be08d8526&=&format=webp&quality=lossless&width=592&height=172",
-              description: "",
-              likes: 0,
-              link: "https://media.discordapp.net/attachments/971299427715272734/1179849642964426873/image.png?ex=657b479b&is=6568d29b&hm=679745322994208e25ce54508ed02ac353cbbeeb1c049b6fb5329b2be08d8526&=&format=webp&quality=lossless&width=592&height=172"
-
-            },
           ]
         },
       ],
